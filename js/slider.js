f56854d28814bf4fb2f48bf8032ea27280b4aba8
Mateusz Mika <mateuszmikla@wp.pl>
2021-03-23 17:41:30 +0100

    a

--- js/slider.js
+++ js/slider.js
@@ -6,7 +6,7 @@ const h3Html = document.querySelector('.container__title');
 const pHtml = document.querySelector('.container__info');
 
 
-const colorImages = ['images/x.jpg', 'images/y.jpg', 'images/z.jpg'];
+const colorImages = ['IMG/x.jpg', 'IMG/y.jpg', 'IMG/z.jpg'];
 const names = ['Firma X', 'Firma Y', 'Firma Z',];
 const opinions = ['Wywodząca sie z X specjalizuje się w produkcji nowoczesnych paneli winylowych, które dzięki wytrzymałości i łatwości montażu zdobywają coraz większe uznanie.', 'Firma Y posiada kilkudziesięcioletnią tradycję w produkcji parkietów i podłóg drewnianych. Jej produkty mimo upływu lat cieszą się dużym zainteresowaniem klientów', 'Firma o międzynarodowym zasięgu, która specjalizuje się w produkcji ekskluzywnych podłóg. Solidne wykonanie i liczna kolekcja wzorów zadowoli gusta najbardziej wymagających klientów'];
 
